9f0f33b16605b15930cb97270b15a049cb308776
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-22 18:31:02 +0300

    refactor Currency component to simplify data fetching and improve rendering logic

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,46 +1,56 @@
 import { useEffect, useState } from "react";
-import fetchCurrency from "../../services/monobankApi.js";
-import CurrencyTable from "../CurrencyTable/CurrencyTable.jsx";
-import parseCurrency from "../../utils/parseCurrency.js";
-import CurrencyChart from "../CurrencyChart/CurrencyChart.jsx";
+import s from "./Currency.module.css";
+import CurrencyChart from "../CurrencyChart/CurrencyChart";
 
-const ONE_HOUR_MS = 60 * 60 * 1000;
+import { useMedia } from "../../hooks/useMedia";
+import { getCurrency } from "../../services/monobankApi.js";
 
 const Currency = () => {
-  const [currencyUpdated, setCurrencyUpdated] = useState(() =>
-    localStorage.getItem("currencyUpdatedAt")
+  const [currency, setCurrency] = useState(
+    () => JSON.parse(localStorage.getItem("currency")) || ""
   );
-  const [usdEurCurrency, setUsdEurCurrency] = useState(() =>
-    JSON.parse(localStorage.getItem("usdEurCurrency"))
-  );
-
   useEffect(() => {
-    if (!currencyUpdated || currencyUpdated < Date.now() - ONE_HOUR_MS) {
-      (async () => {
-        const currency = await fetchCurrency();
-
-        if (!currency) return;
-
-        const parsedCurrency = parseCurrency(currency);
-
-        setUsdEurCurrency(parsedCurrency);
-        localStorage.setItem("usdEurCurrency", JSON.stringify(parsedCurrency));
-
-        const currencyUpdatedAt = Date.now();
-        setCurrencyUpdated(currencyUpdatedAt);
-        localStorage.setItem("currencyUpdatedAt", currencyUpdatedAt);
-      })();
-    }
-  }, [currencyUpdated, usdEurCurrency]);
-
-  if (!usdEurCurrency) {
-    return <div>Loading...</div>;
-  }
+    const getData = async () => {
+      const data = await getCurrency();
+      setCurrency(data);
+    };
+    getData();
+  }, []);
+  const { isDesktop, isTablet } = useMedia();
 
   return (
     <>
-      <CurrencyTable currencyData={usdEurCurrency} />
-      <CurrencyChart currencyData={usdEurCurrency} />
+      {currency && (
+        <div className={s.wrapper}>
+          <table className={s.table}>
+            <thead>
+              <tr>
+                <td>Currency</td>
+                <td>Purchase</td>
+                <td>Sale</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>USD</td>
+                <td>{currency.usd.buy}</td>
+                <td>{currency.usd.sell}</td>
+              </tr>
+              <tr>
+                <td>EUR</td>
+                <td>{currency.eur.buy}</td>
+                <td>{currency.eur.sell}</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <CurrencyChart
+            usd={currency.usd.buy}
+            eur={currency.eur.buy}
+            type={isDesktop ? "desk" : isTablet ? "tab" : "mob"}
+          />
+        </div>
+      )}
     </>
   );
 };
