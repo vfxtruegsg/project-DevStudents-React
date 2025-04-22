6daa0437e8ec4f5dba28a1c4c9047116489f5688
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-22 18:30:29 +0300

    refactor currency fetching logic to use axios instance and improve error handling

--- src/services/monobankApi.js
+++ src/services/monobankApi.js
@@ -1,13 +1,37 @@
 import axios from "axios";
+const instance = axios.create({
+  baseURL: "https://api.monobank.ua/bank/currency",
+});
 
-const CURRENCY_API_URL = "https://api.monobank.ua/bank/currency";
+const fetchCurrency = async () => {
+  const { data } = await instance.get();
+  return data;
+};
 
-export default async function fetchCurrency() {
-  try {
-    const { data } = await axios.get(CURRENCY_API_URL);
-    return data;
-  } catch (error) {
-    console.warn(error.message);
-    return null;
+export const getCurrency = async () => {
+  const cachedCurrency = JSON.parse(localStorage.getItem("currency"));
+  const now = Date.now();
+
+  if (cachedCurrency && now - cachedCurrency.date < 3600000) {
+    return cachedCurrency;
   }
-}
+
+  try {
+    const data = await fetchCurrency();
+    const usd = data.find(
+      (item) => item.currencyCodeA === 840 && item.currencyCodeB === 980
+    );
+    const eur = data.find(
+      (item) => item.currencyCodeA === 978 && item.currencyCodeB === 980
+    );
+    const currencyData = {
+      date: now,
+      usd: { buy: usd.rateBuy.toFixed(2), sell: usd.rateSell.toFixed(2) },
+      eur: { buy: eur.rateBuy.toFixed(2), sell: eur.rateSell.toFixed(2) },
+    };
+    localStorage.setItem("currency", JSON.stringify(currencyData));
+    return currencyData;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
