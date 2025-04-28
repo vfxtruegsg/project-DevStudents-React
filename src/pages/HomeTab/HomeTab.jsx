bd81db9413fa14a022ee5fd0d419ebc2089fc406
shura73 <ol.chegrynets@gmail.com>
2025-04-28 13:59:10 +0200

    Added home to hometap

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,13 +1,13 @@
 import ButtonAddTransactions from "../../components/ButtonAddTransactions/ButtonAddTransactions";
 import Home from "../../components/Home/Home.jsx";
 
-import s from "./HomeTab.module.css";
+import css from "./HomeTab.module.css";
 
 const HomeTab = () => {
   return (
     <>
-      <Home
       <ButtonAddTransactions />
+      <Home />
     </>
   );
 };
